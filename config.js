da1187971b76cf34c115e351005a8a5271dfc6a4
imeshkawshalya <155098034+imeshkawshalya@users.noreply.github.com>
2024-11-18 20:50:23 +0530

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 
 module.exports = {
   //==========================================- MAIN - CONFIGS -==================================================================
-  SESSION_ID: process.env.SESSION_ID || "YblknQ6K#mnmK-A7I5hv0K_vySYHnH3JmKsGEfashA77v-IRuoUs",
+  SESSION_ID: process.env.SESSION_ID || "JOthWYAS#woSYVolNRDSs7lc1a53GTd8CBDMJLTdk5K_642D89jI",
   // ADD Your Session Id 
   MONGODB: process.env.MONGODB || "mongodb+srv://kulathungaasitha319:yjHB0DvFfStNfwPS@cluster0.3oijd.mongodb.net/",
     // ADD Your MongoDB Database URL
